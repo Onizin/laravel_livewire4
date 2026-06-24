b30f8af7bed35c85a6dac15b8ec953cf6ed8acfa
Wahyu Ismaya CM <86389030+Onizin@users.noreply.github.com>
2026-06-25 00:10:35 +0700

    Add kategori data to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        //kategori
         $kategoris = [
             ['nama' => 'Elektronik', 'deskripsi' => 'Produk elektronik'],
             ['nama' => 'Makanan',    'deskripsi' => 'Produk makanan'],
