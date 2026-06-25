a79d48615639adbb0db99b429a592926f4beede1
Wahyu Ismaya CM <86389030+Onizin@users.noreply.github.com>
2026-06-25 22:01:50 +0700

    Add create category modal in create.blade.php

---
+++ resources/views/livewire/superadmin/kategori/create.blade.php
@@ -0,0 +1,35 @@
+<div wire:ignore.self class="modal fade" id="createModalKategori" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-md modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah {{$title}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+            <label for="nama" class="form-label">Nama</label>
+            <span class="text-danger">*</span>
+            <input wire:model="nama" type="text" class="form-control" placeholder="Nama Kategori">
+            @error('nama')
+                <small class="text-danger">{{$message}}</small>
+            @enderror
+        </div>
+        <div class="row mt-2">
+            <label for="deskripsi" class="form-label">Deskripsi</label>
+            <span class="text-danger">*</span>
+            <input wire:model="deskripsi" type="text" class="form-control" placeholder="Deskripsi Kategori">
+
+            @error('deskripsi')
+                <small class="text-danger">{{$message}}</small>
+            @enderror
+        </div>
+       </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-times mr-1"></i>Close</button>
+        <button type="button" class="btn btn-primary" wire:click="store"><i class="fas fa-save mr-1"></i>Save</button>
+      </div>
+    </div>
+  </div>
+</div>
